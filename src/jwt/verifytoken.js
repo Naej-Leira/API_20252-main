f9a479b29b4eef691e6593dbd19def4c59b02488
Naej-Leira <naej.leira02@gmail.com>
2025-11-09 00:23:44 -0500

    Actualizo la API: mejoras en rutas y controladores

--- src/jwt/verifytoken.js
+++ src/jwt/verifytoken.js
@@ -4,7 +4,7 @@ import { JWT_SECRET } from '../config.js';
 // Token estático por defecto (tu token actual)eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
 
 
-const DEFAULT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzYyNTY5NDc5LCJleHAiOjE3NjI2NTU4Nzl9.cSXCDMRRmjBA9T2G-sFLre68X1-6mrfyIUyj_F0tU2g";
+const DEFAULT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzYyNjMwNzA0LCJleHAiOjE3NjI3MTcxMDR9.q42vrDAOi9xNaRszUcCRDALCulwRQEQUvRQWA4MGryg";
 
 export const verifyToken = async (req, res, next) => {
     let token;
